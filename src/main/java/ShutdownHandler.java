29c4a1fc231d71c0f4513b1393478628d69af5c0
Ben <brsandvik@gmail.com>
2020-09-11 12:15:58 -0700

    Made the ShutdownHandler actually work by default, you should still modify it to suit your needs

--- src/main/java/ShutdownHandler.java
+++ src/main/java/ShutdownHandler.java
@@ -1,6 +1,11 @@
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.events.ShutdownEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
 
-public class ShutdownHandler {
+public class ShutdownHandler extends ListenerAdapter {
+    private static volatile boolean shutdown;
+
     /**
      * Go nuts. Also feel free to include properties in this class if you're having trouble
      * getting all the data you need in this method, I'm not your parents.
@@ -8,6 +13,29 @@ public class ShutdownHandler {
      * @param jda the JDA instance
      */
     public static void handle(JDA jda){
+        System.out.println("Shutting down...");
+        jda.addEventListener(new ShutdownHandler());
         jda.shutdown();
+        // YOUR CODE HERE
+
+
+        // YOUR CODE SHOULD PROBABLY END HERE
+        int count = 0;
+        try{
+            while(!shutdown){
+                count++;
+                Thread.sleep(10);
+            }
+        }
+        catch (InterruptedException e){
+            System.out.println("Shutdown interrupted after " + count + " tries.");
+            return;
+        }
+        System.out.println("Shutdown properly after " + count + " tries.");
+    }
+
+    @Override
+    public void onShutdown(@NotNull ShutdownEvent event) {
+        shutdown = true;
     }
 }
